59ca46d9b94ab30d79531d4247373aa8c4c1a206
Priyanka <priyanka.kumari@hyperisland.se>
2025-01-30 19:45:26 +0100

    Initial commit for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import './App.css';
-import CoctailsList from './Components/API Component/API Component';
+// import CoctailsList from './Components/API Component/API Component';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header Component/Header";
+
+const Homepage = () => <h2>Welcome to Homepage</h2>;
+const Favorites = () => <h2>My Favourites</h2>;
+const SignOut = () => <h2>Signing Out...</h2>;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Some text
-
-        <div className='test'>Recipes</div>
-
-      </header>
-      <div>
-      <CoctailsList />
+       <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/signout" element={<SignOut />} />
+      </Routes>
+    </Router>
     </div>
-    </div>
+
+    
   );
 }
 
