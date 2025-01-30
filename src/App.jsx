1e2606d01f5889f2fc63cb97852d47c511c312b4
Priyanka <priyanka.kumari@hyperisland.se>
2025-01-30 21:08:46 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,37 @@
-
-import './App.css';
-// import CoctailsList from './Components/API Component/API Component';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header Component/Header";
+import Footer from "./Components/Footer Component/Footer";
+
+import "./App.css";
+import SearchHeader from "./Components/SearchHeaderComponent/SearchHeaderComponent";
+import CocktailList from "./Components/Cocktail-list-component-AR/Cocktail-list.jsx";
+import { ProductCart } from "./Components/SingleProductCart/ProductCart";
 
 const Homepage = () => <h2>Welcome to Homepage</h2>;
 const Favorites = () => <h2>My Favourites</h2>;
 const SignOut = () => <h2>Signing Out...</h2>;
-import "./App.css";
-import SearchHeader from './Components/SearchHeaderComponent/SearchHeaderComponent';
-import CocktailList from './Components/Cocktail-list-component-AR/Cocktail-list.jsx';
-import { ProductCart } from "./Components/SingleProductCart/ProductCart";
 
 function App() {
   return (
     <div className="App">
-
-       <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/signout" element={<SignOut />} />
-      </Routes>
-    </Router>
-
-
-  
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/signout" element={<SignOut />} />
+        </Routes>
+      </Router>
 
       <main>
-        <SearchHeader/>
-        <CocktailList/>
+        <SearchHeader />
+        <CocktailList />
         <ProductCart />
       </main>
 
-      {/* footer */}
-
-
+      <Footer />
     </div>
-
-    
   );
 }
 
