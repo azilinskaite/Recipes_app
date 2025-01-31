586fe42b871ad566d7e86a5bca528fbbc862ca83
Anita-Kumari1987 <anita.kumari1987@yahoo.com>
2025-01-31 14:15:14 +0100

    Added heart icon and did CSS

--- src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
+++ src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import './Cocktail-list.css'; 
+import "./Cocktail-list.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const CocktailsList = () => {
   const [cocktail, setCocktail] = useState([]);
@@ -8,22 +10,38 @@ const CocktailsList = () => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
       .then((response) => response.json())
       .then((data) => {
-        setCocktail(data.drinks); 
+        setCocktail(data.drinks);
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
-    <div className="cocktail-grid"> {}
+    <div className="cocktail-grid">
+      {" "}
+      {}
       {cocktail.map((drink) => (
-        <div key={drink.idDrink} className="productCartContainer"> {}
-        <img
+        <div key={drink.idDrink} className="productCartContainer">
+          {" "}
+          {}
+          <img
             src={drink.strDrinkThumb}
             alt={drink.strDrink}
             style={{ width: "100%", height: "auto" }}
           />
-          <h2>{drink.strDrink}</h2>
-          <p>{drink.strCategory}</p>
+          <div className="infoDiv">
+            <div className="textContainer">
+              <h2>{drink.strDrink}</h2>
+              <p>{drink.strCategory}</p>
+            </div>
+            <div className="iconDiv">
+              <FontAwesomeIcon
+                icon={faHeart}
+                size="2x"
+                color="red"
+                className="heart"
+              />
+            </div>
+          </div>
         </div>
       ))}
     </div>
