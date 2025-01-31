c3cb1853204ee73fc069189430d5bddc42bd5918
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-01-31 10:19:43 +0100

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,36 @@
-
-import './App.css';
-// import CoctailsList from './Components/API Component/API Component';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header Component/Header";
+import SearchHeader from "./Components/SearchHeaderComponent/SearchHeaderComponent";
+import CocktailList from "./Components/Cocktail-list-component-AR/Cocktail-list.jsx";
+import { ProductCart } from "./Components/SingleProductCart/ProductCart";
 
 const Homepage = () => <h2>Welcome to Homepage</h2>;
 const Favorites = () => <h2>My Favourites</h2>;
 const SignOut = () => <h2>Signing Out...</h2>;
-import "./App.css";
-import SearchHeader from './Components/SearchHeaderComponent/SearchHeaderComponent';
-import CocktailList from './Components/Cocktail-list-component-AR/Cocktail-list.jsx';
-import { ProductCart } from "./Components/SingleProductCart/ProductCart";
 
 function App() {
   return (
     <div className="App">
-
-       <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/signout" element={<SignOut />} />
-      </Routes>
-    </Router>
-
-
-  
+      <header>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/signout" element={<SignOut />} />
+          </Routes>
+        </Router>
+      </header>
 
       <main>
-        <SearchHeader/>
-        <CocktailList/>
+        <SearchHeader />
+        <CocktailList />
         <ProductCart />
       </main>
 
-      {/* footer */}
-
-
+      {/* <footer></footer> */}
     </div>
-
-    
   );
 }
 
