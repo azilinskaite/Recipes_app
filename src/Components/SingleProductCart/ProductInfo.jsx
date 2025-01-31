0a419926604007f3a8d068863caea87bc81363d0
Anita-Kumari1987 <anita.kumari1987@yahoo.com>
2025-01-31 16:05:27 +0100

    stylling

--- src/Components/SingleProductCart/ProductInfo.jsx
+++
@@ -1,9 +0,0 @@
-const ProductInfo = () => {
-  return (
-    <>
-      <h2>Cocktail Name</h2>
-      <p>ingredient list: votka, soda, juice, suger njhnm </p>
-    </>
-  );
-};
-export default ProductInfo;
