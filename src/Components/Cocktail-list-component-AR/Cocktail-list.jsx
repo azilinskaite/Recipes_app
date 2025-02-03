02408a58c1bd1a47ae7c19151b7872033d9b2e98
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-02-03 11:03:54 +0100

    Add to favourites function

--- src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
+++ src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-
-import './Cocktail-list.css'; 
 import SearchHeader from '../SearchHeaderComponent/SearchHeaderComponent.jsx';
+import { useFavourites } from '../FavouritesContext/FavouritesContext.jsx';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 
 const CocktailsList = () => {
   const [cocktail, setCocktail] = useState([]);
+  const { favourites, addToFavourites, removeFromFavourites } = useFavourites();
 
   useEffect(() => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
@@ -17,6 +18,15 @@ const CocktailsList = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const toggleFavourite = (drink) => {
+    const isFavourite = favourites.some(fav => fav.idDrink === drink.idDrink);
+    if (isFavourite) {
+      removeFromFavourites(drink.idDrink);
+    } else {
+      addToFavourites(drink);
+    }
+  };
+
   return (
     <section>
       <SearchHeader/>
@@ -25,8 +35,6 @@ const CocktailsList = () => {
       {}
       {cocktail.map((drink) => (
         <div key={drink.idDrink} className="productCartContainer">
-          {" "}
-          {}
           <img
             src={drink.strDrinkThumb}
             alt={drink.strDrink}
@@ -34,16 +42,16 @@ const CocktailsList = () => {
           />
           <div className="iconContainer">
             <h2>{drink.strDrink}</h2>
-            <button className="iconDiv">
-              <FontAwesomeIcon
-                icon={faHeart}
-                size="2x"
-                color="red"
-                className="heart"
-              />
+            <button className="iconDiv" onClick={() => toggleFavourite(drink)}>
+            <FontAwesomeIcon
+                  icon={favourites.some(fav => fav.idDrink === drink.idDrink) ? faHeartSolid : faHeartRegular}
+                  size="2x"
+                  color="red"
+                  className="heart"
+                />
             </button>
           </div>
-          <p>{drink.strCategory}</p>
+          <p>{drink.strIngredient}</p>
         </div>
       ))}
     </div>
