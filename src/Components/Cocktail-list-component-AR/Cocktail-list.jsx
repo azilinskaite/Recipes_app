bee4ac8d1a2fa7c20313202e793766b693f6761c
Priyanka <priyanka.kumari@hyperisland.se>
2025-02-03 14:09:06 +0100

    Search Navigation functionality implemneted

--- src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
+++ src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
@@ -1,32 +1,61 @@
 import React, { useState, useEffect } from "react";
-import './Cocktail-list.css'; 
+import SearchHeader from "../SearchHeaderComponent/SearchHeaderComponent.jsx";
+import { useFavourites } from "../FavouritesContext/FavouritesContext.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 
-const CocktailsList = () => {
-  const [cocktail, setCocktail] = useState([]);
+const CocktailsList = ({ items = [] }) => {
+  //const [cocktail, setCocktail] = useState([]);
+  //const validItems = Array.isArray(items) ? items : [];
+  //const validItems = Array.isArray(items) && items.length > 0 ? items : cocktail;
+  const { favourites, addToFavourites, removeFromFavourites } = useFavourites();
 
-  useEffect(() => {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
-      .then((response) => response.json())
-      .then((data) => {
-        setCocktail(data.drinks); 
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  const toggleFavourite = (drink) => {
+    const isFavourite = favourites.some((fav) => fav.idDrink === drink.idDrink);
+    if (isFavourite) {
+      removeFromFavourites(drink.idDrink);
+    } else {
+      addToFavourites(drink);
+    }
+  };
 
   return (
-    <div className="cocktail-grid"> {}
-      {cocktail.map((drink) => (
-        <div key={drink.idDrink} className="productCartContainer"> {}
-          <h2>{drink.strDrink}</h2>
-          <p>{drink.strCategory}</p>
-          <img
-            src={drink.strDrinkThumb}
-            alt={drink.strDrink}
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-      ))}
-    </div>
+    <section>
+      {/* <SearchHeader/> */}
+      <div className="cocktail-grid">
+        {" "}
+        {}
+        {items.map((drink) => (
+          <div key={drink.idDrink} className="productCartContainer">
+            <img
+              src={drink.strDrinkThumb}
+              alt={drink.strDrink}
+              style={{ width: "100%", height: "auto" }}
+            />
+            <div className="iconContainer">
+              <h2>{drink.strDrink}</h2>
+              <button
+                className="iconDiv"
+                onClick={() => toggleFavourite(drink)}
+              >
+                <FontAwesomeIcon
+                  icon={
+                    favourites.some((fav) => fav.idDrink === drink.idDrink)
+                      ? faHeartSolid
+                      : faHeartRegular
+                  }
+                  size="2x"
+                  color="red"
+                  className="heart"
+                />
+              </button>
+            </div>
+            <p>{drink.strIngredient}</p>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
