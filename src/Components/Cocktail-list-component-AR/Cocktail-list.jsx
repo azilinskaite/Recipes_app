9d06c1d8ea64062aef585868ef6eaa8e23e01eee
Anita-Kumari1987 <anita.kumari1987@yahoo.com>
2025-02-03 15:52:30 +0100

    Adding recipe feature

--- src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
+++ src/Components/Cocktail-list-component-AR/Cocktail-list.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import SearchHeader from '../SearchHeaderComponent/SearchHeaderComponent.jsx';
-import { useFavourites } from '../FavouritesContext/FavouritesContext.jsx';
+import SearchHeader from "../SearchHeaderComponent/SearchHeaderComponent.jsx";
+import { useFavourites } from "../FavouritesContext/FavouritesContext.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+import { Recipe } from "../Recipes/Recipe.jsx";
 
 const CocktailsList = () => {
   const [cocktail, setCocktail] = useState([]);
@@ -19,42 +20,57 @@ const CocktailsList = () => {
   }, []);
 
   const toggleFavourite = (drink) => {
-    const isFavourite = favourites.some(fav => fav.idDrink === drink.idDrink);
+    const isFavourite = favourites.some((fav) => fav.idDrink === drink.idDrink);
     if (isFavourite) {
       removeFromFavourites(drink.idDrink);
     } else {
       addToFavourites(drink);
     }
   };
-
+  const [showComponent, setshowComponent] = useState(false);
   return (
     <section>
-      <SearchHeader/>
-    <div className="cocktail-grid">
-      {" "}
-      {}
-      {cocktail.map((drink) => (
-        <div key={drink.idDrink} className="productCartContainer">
-          <img
-            src={drink.strDrinkThumb}
-            alt={drink.strDrink}
-            style={{ width: "100%", height: "auto" }}
-          />
-          <div className="iconContainer">
-            <h2>{drink.strDrink}</h2>
-            <button className="iconDiv" onClick={() => toggleFavourite(drink)}>
-            <FontAwesomeIcon
-                  icon={favourites.some(fav => fav.idDrink === drink.idDrink) ? faHeartSolid : faHeartRegular}
-                  size="2x"
-                  color="red"
-                  className="heart"
-                />
-            </button>
-          </div>
-          <p>{drink.strIngredient}</p>
-        </div>
-      ))}
-    </div>
+      <SearchHeader />
+      <div className="cocktail-grid">
+        {" "}
+        {}
+        {cocktail.map((drink) =>
+          !showComponent ? (
+            <div
+              key={drink.idDrink}
+              className="productCartContainer"
+              onClick={() => setshowComponent(true)}
+            >
+              <img
+                src={drink.strDrinkThumb}
+                alt={drink.strDrink}
+                style={{ width: "100%", height: "auto" }}
+              />
+              <div className="iconContainer">
+                <h2>{drink.strDrink}</h2>
+                <button
+                  className="iconDiv"
+                  onClick={() => toggleFavourite(drink)}
+                >
+                  <FontAwesomeIcon
+                    icon={
+                      favourites.some((fav) => fav.idDrink === drink.idDrink)
+                        ? faHeartSolid
+                        : faHeartRegular
+                    }
+                    size="2x"
+                    color="red"
+                    className="heart"
+                  />
+                </button>
+              </div>
+              <p>{drink.strIngredient}</p>
+            </div>
+          ) : (
+            <Recipe />
+          )
+        )}
+      </div>
     </section>
   );
 };
---
+++ src/Components/Recipes/Recipe.jsx
@@ -0,0 +1,10 @@
+import "./Recipe.css";
+
+export const Recipe = () => {
+  return (
+    <div className="recipe_container">
+      <p>Ingredient</p>
+      <p>Instructions</p>
+    </div>
+  );
+};
