0ceb79d2869e0888a81766918bfe7ca79bc7addf
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-02-04 14:50:11 +0100

    Footer position fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header Component/Header";
-import DynamicHeader from './Components/SearchHeaderComponent/DynamicHeader';
+import DynamicHeader from "./Components/SearchHeaderComponent/DynamicHeader";
 import CocktailsList from "./Components/Cocktail-list-component-AR/Cocktail-list.jsx";
 import Footer from "./Components/Footer Component/Footer.jsx";
 import { FavouritesProvider } from "./Components/FavouritesContext/FavouritesContext.jsx";
 import FavouritesList from "./Components/FavouritesListComponent/FavouritesListComponent.jsx";
 import { CocktailDetails } from "./Components/Recipes/Recipe.jsx";
-// import SearchByName from "./Components/CocktailSearch Component/SearchByName.jsx";
-
-// const Homepage = () => (
-//   <div>
-//   </div>
-// );
 
 const SignOut = () => <h2>Signing Out...</h2>;
 
@@ -22,26 +16,30 @@ function App() {
       <Router>
         <div className="App">
           <Header />
-          <Routes>
-            <Route path="/" element={
-              <>
-                <DynamicHeader type="search" />
-                <CocktailsList />
-              </>
-            } />
-            <Route path="/favorites" element={
-              <>
-                <DynamicHeader type="favorites" />
-                <FavouritesList />
-              </>
-            } />
-            <Route path="/signout" element={<SignOut />} />
-              </Routes>
-               <Routes>
-              <Route path="/" element={<CocktailsList />} />
+          <div className="content">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <DynamicHeader type="search" />
+                    <CocktailsList />
+                  </>
+                }
+              />
+              <Route
+                path="/favorites"
+                element={
+                  <>
+                    <DynamicHeader type="favorites" />
+                    <FavouritesList />
+                  </>
+                }
+              />
+              <Route path="/signout" element={<SignOut />} />
               <Route path="/cocktail/:id" element={<CocktailDetails />} />
             </Routes>
-          
+          </div>
           <Footer />
         </div>
       </Router>
